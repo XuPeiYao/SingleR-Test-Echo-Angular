bff080fc51975a7fe5ef3a6c90c48cf4901d237f
XuPeiYao <st89382000@gmail.com>
2018-04-29 23:30:16 +0800

    加入簡易echo測試

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as signalr from '@aspnet/signalr';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  private hubConnection: signalr.HubConnection;
+
+  message: string;
+
+
+  ngOnInit() {
+    this.hubConnection = new signalr.HubConnection('/echo');
+
+    this.hubConnection
+      .start()
+      .then(() => {
+        console.log('Connection started!');
+        this.hubConnection.on('ReceiveMessage', (message) => { // 註冊本地事件
+          alert(message);
+        });
+      })
+      .catch(err => console.log('Error while establishing connection :('));
+
+  }
+
+  submit() {
+    this.hubConnection.send('SendMessage', this.message); // 調用EchoHub的SendMessage方法
+    this.message = '';
+  }
 }
